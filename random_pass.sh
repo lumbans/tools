b7983acb1fb6e4e09a927b5d879447597124668c
Lumban Sopian <lumban.so7@gmail.com>
2023-07-18 17:37:36 +0700

    put command for sample output

--- random_pass.sh
+++ random_pass.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+# This is to create random password or number in macOS terminal
+# sample output
+# PK6#m&NN5t!FQjXenb*E
+# kMNeuuTm(AtIL(;ZW6d4
+# k5Fyd83Gi3te!o4frCL0
+# K4Q?;nGvgi@6iX498dky
+# Tpm5$sx0fJz5Xbr)=_b(
+# SEi5EBabUd=LTGWwKM;v
+# +sEHysngb7HR&rfSU(31
+# LovvJqCKt&jOlh(9=+8O
+# K^*bk8zB2HHRmB*;&uwp
+# BT&=INTOD#@rhUPr=4_P
+
+
 i="0"
 
 while [[ $i -lt 10 ]];
